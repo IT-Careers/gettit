3fdde92a30ff2184dba5700184f3c9b253cec5e5
Ivaylo Jelev <atriumandvintrigol@gmail.com>
2024-12-14 12:08:55 +0200

    GET-11 | GET-14 Implemented Generic repository and added Attachment (#4)
    * GET-11 | GET-14 Implemented Generic repository and added Attachment
    
    * GET-11 | GET-14 Fixed launchSettings

--- src/Data/Gettit.Data.Models/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Gettit.Data.Models
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ src/Data/Gettit.Data/Repositories/AttachmentRepository.cs
@@ -0,0 +1,12 @@
+﻿using Gettit.Data.Models;
+using Gettit.Web.Data;
+
+namespace Gettit.Data.Repositories
+{
+    public class AttachmentRepository : BaseGenericRepository<Attachment>
+    {
+        public AttachmentRepository(GettitDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
