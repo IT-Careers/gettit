f17f297639cb9f6974752f0da4245a16102165c1
DCay <atriumandvintrigol@gmail.com>
2024-12-14 12:06:48 +0200

    GET-11 | GET-14 Implemented Generic repository and added Attachment

---
+++ src/Data/Gettit.Data/Repositories/BaseGenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using Gettit.Data.Models;
+using Gettit.Web.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gettit.Data.Repositories
+{
+    public abstract class BaseGenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : BaseEntity
+    {
+        protected readonly GettitDbContext _dbContext;
+
+        protected BaseGenericRepository(GettitDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public async Task<TEntity> CreateAsync(TEntity entity)
+        {
+            await this._dbContext.AddAsync(entity);
+            await this._dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<TEntity> DeleteAsync(TEntity entity)
+        {
+            this._dbContext.Remove(entity);
+            await this._dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<TEntity> EditAsync(TEntity entity)
+        {
+            this._dbContext.Update(entity);
+            await this._dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return this._dbContext.Set<TEntity>().AsQueryable<TEntity>();
+        }
+
+        public IQueryable<TEntity> GetAllAsNoTracking()
+        {
+            return this._dbContext.Set<TEntity>().AsNoTracking();
+        }
+    }
+}
