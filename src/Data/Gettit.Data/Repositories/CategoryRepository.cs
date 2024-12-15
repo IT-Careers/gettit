2ce8b5f60ef78118b4c5fd7288dbaa1779fbc921
Ivaylo Jelev <atriumandvintrigol@gmail.com>
2024-12-15 11:49:05 +0200

    GET-15 Implemented entity repositories (#8)

---
+++ src/Data/Gettit.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Gettit.Data.Repositories
+{
+    using Gettit.Data.Models;
+    using Gettit.Web.Data;
+    using Microsoft.AspNetCore.Http;
+
+    public class CategoryRepository : MetadataBaseGenericRepository<Category>
+    {
+        public CategoryRepository(GettitDbContext dbContext, IHttpContextAccessor httpContextAccessor) 
+            : base(dbContext, httpContextAccessor)
+        {
+        }
+    }
+}
---
+++ src/Data/Gettit.Data/Repositories/GettitRoleRepository.cs
@@ -0,0 +1,14 @@
+﻿using Gettit.Data.Models;
+using Gettit.Web.Data;
+using Microsoft.AspNetCore.Http;
+
+namespace Gettit.Data.Repositories
+{
+    public class GettitRoleRepository : MetadataBaseGenericRepository<GettitRole>
+    {
+        public GettitRoleRepository(GettitDbContext dbContext, IHttpContextAccessor httpContextAccessor) 
+            : base(dbContext, httpContextAccessor)
+        {
+        }
+    }
+}
